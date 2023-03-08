4185cabfc6cc52639d3b8fd7b7b6a1a2b6b5e883
edison1105 <daiwei521@126.com>
2023-03-08 22:12:53 +0800

    fix(cssVars): should keep backslashe in ssr

--- packages/compiler-sfc/src/cssVars.ts
+++ packages/compiler-sfc/src/cssVars.ts
@@ -20,9 +20,12 @@ export function genCssVarsFromList(
   isSSR = false
 ): string {
   return `{\n  ${vars
-    .map(
-      key => `"${isSSR ? `--` : ``}${genVarName(id, key, isProd)}": (${key})`
-    )
+    .map(key => {
+      const varName = genVarName(id, key, isProd)
+      return `"${isSSR ? `--` : ``}${
+        isSSR ? varName.replace(/\\/, '\\\\') : varName
+      }": (${key})`
+    })
     .join(',\n  ')}\n}`
 }
 
