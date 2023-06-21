a5b9a65c528699b0fbb5222b50eecdc43e06a2d1
daiwei <daiwei521@126.com>
2023-06-21 17:54:38 +0800

    WIP: support children override for ssr custom dir

--- packages/server-renderer/src/helpers/ssrGetDirectiveProps.ts
+++ packages/server-renderer/src/helpers/ssrGetDirectiveProps.ts
@@ -1,26 +1,53 @@
-import { ComponentPublicInstance, Directive } from '@vue/runtime-core'
+import {
+  ComponentPublicInstance,
+  Directive,
+  mergeProps
+} from '@vue/runtime-core'
+import { escapeHtml } from '@vue/shared'
+
+type DirType = {
+  dir: Directive
+  value?: any
+  arg?: string
+  modifiers: Record<string, boolean>
+}
 
 export function ssrGetDirectiveProps(
   instance: ComponentPublicInstance,
-  dir: Directive,
-  value?: any,
-  arg?: string,
-  modifiers: Record<string, boolean> = {}
-): Record<string, any> {
-  if (typeof dir !== 'function' && dir.getSSRProps) {
-    return (
-      dir.getSSRProps(
-        {
-          dir,
-          instance,
-          value,
-          oldValue: undefined,
-          arg,
-          modifiers
-        },
-        null as any
-      ) || {}
-    )
+  tag: string,
+  dirs: DirType[]
+): [string | undefined, Record<string, any>] {
+  const dirProps: Record<string, any> = {}
+
+  dirs.forEach(({ dir, value, arg, modifiers = {} }) => {
+    if (typeof dir !== 'function' && dir.getSSRProps) {
+      return mergeProps(
+        dirProps,
+        dir.getSSRProps(
+          {
+            dir,
+            instance,
+            value,
+            oldValue: undefined,
+            arg,
+            modifiers
+          },
+          null as any
+        ) as Record<string, any>
+      )
+    }
+  })
+
+  let childrenOverride: string | undefined
+  if (dirProps) {
+    if (dirProps.innerHTML) {
+      childrenOverride = dirProps.innerHTML
+    } else if (dirProps.textContent) {
+      childrenOverride = escapeHtml(dirProps.textContent)
+    } else if (tag === 'textarea' && dirProps.value) {
+      childrenOverride = escapeHtml(dirProps.value)
+    }
   }
-  return {}
+
+  return [childrenOverride, dirProps]
 }
