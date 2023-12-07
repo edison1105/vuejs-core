8d714f7e6df5c591ee5722006d0299d37ae9a313
daiwei <daiwei521@126.com>
2023-12-07 09:15:25 +0800

    fix(runtime-dom): only set height or height of embedded tags as attribute

--- packages/runtime-dom/src/patchProp.ts
+++ packages/runtime-dom/src/patchProp.ts
@@ -100,25 +100,23 @@ function shouldSetAsProp(
     return false
   }
 
+  const tag = el.tagName
   // #1526 <input list> must be set as attribute
-  if (key === 'list' && el.tagName === 'INPUT') {
+  if (key === 'list' && tag === 'INPUT') {
     return false
   }
 
   // #2766 <textarea type> must be set as attribute
-  if (key === 'type' && el.tagName === 'TEXTAREA') {
+  if (key === 'type' && tag === 'TEXTAREA') {
     return false
   }
 
   // #8780 the width or height of embedded tags must be set as attribute
-  if (key === 'width' || key === 'height') {
-    const tag = el.tagName
-    return !(
-      tag === 'IMG' ||
-      tag === 'VIDEO' ||
-      tag === 'CANVAS' ||
-      tag === 'SOURCE'
-    )
+  if (
+    (key === 'width' || key === 'height') &&
+    (tag === 'IMG' || tag === 'VIDEO' || tag === 'CANVAS' || tag === 'SOURCE')
+  ) {
+    return true
   }
 
   // native onclick with string value, must be set as attribute
